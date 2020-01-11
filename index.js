12ea7cd8a31b2faad4b674d7e758660116b83f9a
EdisonBedoya98 <edison.bedoyag@udea.edu.co>
2020-01-11 18:52:31 -0500

    added feature sha256

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express();
+const sha256 = require("js-sha256");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.post('/sha256', (req, res) =>{
+    var info=req.body.info;   
+    var hash = sha256(info);
+    console.log("Hash info = "+hash);
+    res.end(hash);
 });
 
 app.listen(8000, () => {
