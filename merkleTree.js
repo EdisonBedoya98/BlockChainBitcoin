d1112f0bfbdb9964df03a0cfae51abb12e6f02db
EdisonBedoya98 <edison.bedoyag@udea.edu.co>
2020-01-20 21:39:35 -0500

    update

--- merkleTree.js
+++ merkleTree.js
@@ -1,1 +1,23 @@
-const Nodes = require("./Nodes.js");
+const Node = require("./Node.js/index.js");
+
+const root = new Node ("Hash");
+
+class merkleTree {
+
+    constructor(leaves) {
+        this.rootNode = null;
+        this.leaves = leaves;
+        
+    }
+
+    get getRoot(){
+        return this.rootNode;
+    }
+
+    buildTree(){
+        
+    }
+
+
+
+}
