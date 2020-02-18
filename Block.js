0bc48bb621ef47ac44a4242e3b592d31f1667909
edison.bedoyag <edison.bedoyag@udea.edu.co>
2020-02-18 18:00:46 -0500

    update

---
+++ Block.js
@@ -0,0 +1,31 @@
+class Block {
+
+    constructor() {
+        this.hashPrev = null;
+        this.nonce = null;
+        this.hashedTransactions = null;
+    }
+
+    setNonce(nonce) {
+        this.nonce = nonce;
+    }
+    setHashedTransactions(hashedTransactions) {
+        this.hashedTransactions = hashedTransactions;
+    }
+    setHashPrev(hashPrev) {
+        this.hashPrev = hashPrev;
+    }
+    getNonce() {
+        return this.nonce;
+    }
+    getHashedTransactions() {
+        return this.hashedTransactions;
+    }
+    getHashPrev() {
+        return this.hashedTransactions;
+    }
+}
+
+module.exports = {
+    Block
+}
